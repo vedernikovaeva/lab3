742b48d984d722309c3f1ba3c04873567cf49e1a
Yeva <vedernikovaa.yeva@student.uzhnu.edu.ua>
2024-11-18 16:45:23 +0100

    Save changes 2

--- routes/auth.php
+++ routes/auth.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
